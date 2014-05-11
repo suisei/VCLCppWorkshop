631a43da0b337dd88d9d4b6c4370e83e81f8f5f8
suisei <github.suisei@gmail.com>
2014-05-11 16:01:45 +0900

    プロジェクト準備

---
+++ VCLCppWorkshop/VCLCppWorkshop/day1/VCLFunctions.hpp
@@ -0,0 +1,16 @@
+//
+//  VCLFunctions.hpp
+//  VCLCppWorkshop
+//
+//  Created by suisei on 2014/05/11.
+//  Copyright (c) 2014年 Kazuki Kumagai. All rights reserved.
+//
+
+#ifndef VCLCppWorkshop_VCLFunctions_hpp
+#define VCLCppWorkshop_VCLFunctions_hpp
+
+
+
+
+
+#endif
---
+++ VCLCppWorkshop/VCLCppWorkshop/day1/day1Tests.hpp
@@ -0,0 +1,17 @@
+//
+//  day1Tests.hpp
+//  VCLCppWorkshop
+//
+//  Created by suisei on 2014/05/11.
+//  Copyright (c) 2014年 Kazuki Kumagai. All rights reserved.
+//
+
+#ifndef VCLCppWorkshop_day1Tests_hpp
+#define VCLCppWorkshop_day1Tests_hpp
+
+void testDay1()
+{
+    
+}
+
+#endif
